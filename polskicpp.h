6670e25c413ad9b0b66d3e5946f2bd3408c471e4
NOWY2115 <82639144+NOWY2115@users.noreply.github.com>
2023-03-29 16:41:23 +0200

    Upload pliku
    Upload pliku.

---
+++ polskicpp.h
@@ -0,0 +1,45 @@
+#ifndef _POLSKI_CPP_H_
+#define _POLSKI_CPP_H_
+
+/* Definiowanie nazw dla przestrzeni itd. */
+#define uzywajac using
+#define przestrzeni namespace
+
+/* Definiowanie typów zmiennych */
+#define calkowita int
+#define pustka void
+#define podwojna double
+#define plywajaca float
+#define ciag string
+#define charakter char
+#define logiczny bool
+#define prawda true
+#define falsz false
+#define nieprzypisane unsigned
+
+/* Definiowanie funkcji */
+#define koniecl endl
+#define glowna main
+#define wypisz cout
+#define podaj cin
+#define zwroc return
+#define wyjdz exit
+#define calkowita int
+#define ustawlokalne setlocale
+#define LC_TYPC LC_CTYPE
+
+/* Definiowanie innych s³ów kluczowy*/
+#define jesli if
+#define inaczej else
+#define dla for
+#define dopoki while
+#define zrob do
+#define I &&
+#define LUB ||
+#define NIE !
+#define zlam break
+#define kontynuuj continue
+#define przelacznik switch
+#define sprawa case
+
+#endif // _POLSKI_CPP_H_
